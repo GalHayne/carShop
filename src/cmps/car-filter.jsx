8cbfe995496085fe4906944e58c4efd571dd6e4f
Gal <galhayne.gmail.com>
2023-02-03 14:43:30 +0200

    add more filter in car-filter cmp

--- src/cmps/car-filter.jsx
+++ src/cmps/car-filter.jsx
@@ -12,9 +12,18 @@ export class CarFilter extends React.Component {
     }
 
     handleVendorChange = ({ target }) => {
-        console.log(target);
         const { value } = target;
         this.setState({ filterBy: { ...this.state.filterBy, vendor: value } })
+    }
+
+    handleMinSpeedChange = ({ target }) => {
+        const { value } = target;
+        this.setState({ filterBy: { ...this.state.filterBy, minSpeed: value } })
+    }
+
+    handleMaxSpeedChange = ({ target }) => {
+        const { value } = target;
+        this.setState({ filterBy: { ...this.state.filterBy, maxSpeed: value } })
     }
 
     componentDidUpdate() {
@@ -26,10 +35,20 @@ export class CarFilter extends React.Component {
     render() {
         const { vendor, minSpeed, maxSpeed } = this.state.filterBy;
         return <section className="car-filter">
+            Filter By
             <form>
-                <label htmlFor="by-vendor">Vender</label>
+                <label htmlFor="by-vendor">Vender:</label>
                 <input id="by-vendor" type="text" value={vendor} onChange={this.handleVendorChange} />
+            </form>
 
+            <form>
+                <label htmlFor="by-min-speed">Minimum Speed:</label>
+                <input id="by-min-speed" type="number" value={minSpeed} onChange={this.handleMinSpeedChange} />
+            </form>
+
+            <form>
+                <label htmlFor="by-max-speed">Maximum Speed:</label>
+                <input id="by-max-speed" type="number" value={maxSpeed} onChange={this.handleMaxSpeedChange} />
             </form>
         </section>
     }
