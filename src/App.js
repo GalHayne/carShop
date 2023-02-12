e0f4d8569a2025ac0c9f5507e41293eba5612de7
Gal <galhayne@gmail.com>
2023-02-12 16:18:41 +0200

    change class name

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import './App.css';
 
-import { Route, Routes } from 'react-router-dom';
-
 import { CarApp } from './pages/car-app';
 import { HeaderApp } from './cmps/car-header';
 
 function App() {
   return (
     <section className="App">
-      <Route>
-
-        <HeaderApp />
-        <CarApp />
-      </Route>
-
+      <HeaderApp />
+      <CarApp />
     </section>
   );
 }
