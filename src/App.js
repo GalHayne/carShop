31e1e8e664a80be5750cc032dd0be65f149b554d
Gal <galhayne.gmail.com>
2023-02-12 23:50:43 +0200

    add link to other pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 
+import { Home } from './assets/css/pages/home';
+import { About } from './assets/css/pages/about';
+
+import {
+  Route, Routes
+} from 'react-router-dom';
+
 import { CarApp } from './pages/car-app';
 import { HeaderApp } from './cmps/car-header';
 
 function App() {
   return (
-    <section className="App">
+    <section>
       <HeaderApp />
-      <CarApp />
+      <Routes>
+        <Route path="/" element={<CarApp />} />
+        <Route path="/Home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </section>
   );
 }
--- src/cmps/car-header.jsx
+++ src/cmps/car-header.jsx
@@ -1,11 +1,13 @@
+import { NavLink } from "react-router-dom"
+
 export function HeaderApp() {
     return (
         <header className="car-header">
             <h1>CarShop</h1>
             <nav>
-                <span>Main</span>
-                <span>About</span>
-                <span>Car Shop</span>
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')} exact>Car Shop</NavLink>
+                <NavLink to="/home" className={({ isActive }) => (isActive ? 'otherActive' : 'inactive')}>Home</NavLink>
+                <NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>About</NavLink>
             </nav>
 
         </header>
